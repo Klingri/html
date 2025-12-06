23e0d8ec20ea7d4d2b13a93e55fdb2828cf112b8
Klingri <klingri52@gmail.com>
2025-12-06 18:20:39 +0100

    Delete extensions/blackify directory

--- extensions/blackify/background.js
+++
@@ -1,4 +0,0 @@
-// background.js
-// This script currently doesn't perform any actions but is required by manifest v3 for service workers.
-// You could add logic here for more complex extension behavior if needed.
-console.log("Blackout Browser background service worker started.");
